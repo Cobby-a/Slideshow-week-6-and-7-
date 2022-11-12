eb6d2b5bb99e3174cfc1aaf05a7c49c281a1fb67
Cobbina Emmanuel <aduboateng333@gmail.com>
2022-11-12 15:11:43 +0000

    slideshow

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
+function nextpic(n) {
   showSlides(slideIndex += n);
 }
-
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -14,12 +11,12 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("pic");
-  if (n > slides.length){
+  if (n > slides.length) {
     slideIndex = 1
-}
-  if (n < 1){ 
-    slideIndex = slides.length 
-}
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
